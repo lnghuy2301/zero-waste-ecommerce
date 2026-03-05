c5329fc8fcdc5ead5793fcb251bf452a17b296a6
lnghuy2301 <lnghuy2301@gmail.com>
2026-03-05 20:22:03 +0700

    Upgrade: main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,21 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // 1. Cấu hình Prefix cho API (VD: http://localhost:3000/api/v1/products)
+  // Cấu hình Prefix cho API (VD: http://localhost:3000/api/v1/products)
   app.setGlobalPrefix('api/v1');
 
-  // 2. Cấu hình CORS (Cho phép Frontend gọi vào)
+  // ấu hình CORS (Cho phép Frontend gọi vào)
   app.enableCors({
     origin: true, // Trong môi trường Dev cho phép tất cả, Prod sẽ giới hạn domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // 3. Cấu hình Validation Pipe (Kiểm soát dữ liệu đầu vào)
+  // Cấu hình Validation Pipe (Kiểm soát dữ liệu đầu vào)
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Tự động loại bỏ các field không được khai báo trong DTO
@@ -25,7 +27,7 @@ async function bootstrap() {
     }),
   );
 
-  // 4. Cấu hình Swagger (Tài liệu API tự động)
+  // Cấu hình Swagger (Tài liệu API tự động)
   const config = new DocumentBuilder()
     .setTitle('Zero Waste E-commerce API')
     .setDescription('API documentation for Zero Waste Project')
@@ -35,6 +37,11 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  // Cấu hình để serve file tĩnh từ thư mục 'uploads'
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   // 5. Khởi chạy server
   await app.listen(3000);
