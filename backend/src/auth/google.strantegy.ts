a6e9d48c1061e6072e442a5c0fffb43c03f92b7f
Tran Trong Phuc <tphuc290923@gmail.com>
2026-03-18 14:42:40 +0700

    Cap nhat lai du lieu google vao auth

--- backend/src/auth/google.strantegy.ts
+++ backend/src/auth/google.strantegy.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@nestjs/common';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     constructor() {
         super({
-            clientID: process.env.GOOGLE_CLIENT_ID || '876021013983-6sdt8v330dtc2sh57433rgk19orb80ee.apps.googleusercontent.com',         // Lấy từ Google Cloud Console
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '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', // Lấy từ Google Cloud Console
+            clientID: process.env.GOOGLE_CLIENT_ID || '',         // Lấy từ Google Cloud Console
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '', // Lấy từ Google Cloud Console
             callbackURL: 'http://localhost:3000/api/v1/auth/google/callback',
             scope: ['email', 'profile'],
         });
