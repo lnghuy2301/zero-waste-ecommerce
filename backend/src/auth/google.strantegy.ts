55740db0328df4a6a3b4e7892651ae468594422b
Tran Trong Phuc <tphuc290923@gmail.com>
2026-03-18 14:44:49 +0700

    no edit

--- backend/src/auth/google.strantegy.ts
+++ backend/src/auth/google.strantegy.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@nestjs/common';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     constructor() {
         super({
-            clientID: process.env.GOOGLE_CLIENT_ID || '',         // Lấy từ Google Cloud Console
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '', // Lấy từ Google Cloud Console
+            clientID: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
             callbackURL: 'http://localhost:3000/api/v1/auth/google/callback',
             scope: ['email', 'profile'],
         });
